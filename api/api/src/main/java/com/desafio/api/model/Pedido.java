abd089bc08c989dee905a23f41272c8683fde191
Priscila Pinheiro <priscilla0101001@gmail.com>
2026-02-15 17:35:03 -0300

    note: Correcting errors

--- api/api/src/main/java/com/desafio/api/model/Pedido.java
+++ api/api/src/main/java/com/desafio/api/model/Pedido.java
@@ -1,11 +1,14 @@
 package com.desafio.api.model;
 
 import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.math.BigDecimal;
+import java.util.List;
 
 @Entity
 @Getter
@@ -22,11 +25,12 @@ public class Pedido {
     private Cliente cliente;
 
     @Column(name = "valor_total")
-    private BigDecimalmal valorTotal;
+    private BigDecimal valorTotal;
 
     @Column(name = "data_pedido")
-    private LocalDateTime dataPedido = LocalDatetime.now();
+    private LocalDateTime dataPedido = LocalDateTime.now();
 
     @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
-    private List<itemPedido> itens;
+    private List<ItemPedido> itens;
+
 }
